20ffd8d3be0dcb35a67ab45a25a9958513e93a4d
Zach Parker <parkerz410@gmail.com>
2019-05-20 21:46:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+ if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else array.length > 2
+   newarray = array.insert(-1, "and #{array[-1]}")
+   newarray.delete_at(-2)
+   newarray.join(", ")
+ end
 end
