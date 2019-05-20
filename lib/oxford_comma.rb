efa9d2bc04a5a5bf48049e6f7ec2a61c5cdda054
Zach Parker <parkerz410@gmail.com>
2019-05-20 21:32:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,11 @@
 
 
 def oxford_comma(array)
-
+ if array.length == 1
+   array.join
+ elsif array.length == 2
+   array.join(" and ")
+ else array.length > 2
+   array.insert(-2, "and")
+   array.join(", ")
 end
