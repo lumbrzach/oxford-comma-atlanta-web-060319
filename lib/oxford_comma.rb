f226bac55e3c88e8af50223cf2d5df8191d4105a
Zach Parker <parkerz410@gmail.com>
2019-05-20 21:13:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,5 @@
 require pry
 
 def oxford_comma(array)
-  if array == [i]
-    array.join
-    binding.pry
-  elsif array == [i, i]
-    array.join(" and ")
-  else array == [i, i, i]
-    array,join(", , and ")
+  
 end
